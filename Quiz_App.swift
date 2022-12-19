2b2c728e7fa11ca1b8f1d424605a1d79a7c8a896
chaolic6505 <chaolic6505@gmail.com>
2022-12-18 18:56:26 -0800

    done

---
+++ Quiz_App.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_AppApp.swift
+//  Quiz App
+//
+//  Created by SC on 2022-12-18.
+//
+
+import SwiftUI
+
+@main
+struct Quiz_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
